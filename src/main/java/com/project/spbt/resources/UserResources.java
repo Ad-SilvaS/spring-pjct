6bba099eb3b5f177dd98e6d2d706e71e1e63393a
Ad-SilvaS <diegosilvaa63@gmail.com>
2025-04-30 14:44:50 -0300

    user entities and resources

---
+++ src/main/java/com/project/spbt/resources/UserResources.java
@@ -0,0 +1,20 @@
+package com.project.spbt.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.spbt.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(12323232L, "John", "jj.a@mail.com", "224442-3333", "sksdjdshd");
+
+        return ResponseEntity.ok().body(u);
+    }
+}
