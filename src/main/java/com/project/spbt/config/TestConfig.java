3eceaee5886a1c3d0e245148dce1b4c979e66b24
Ad-SilvaS <diegosilvaa63@gmail.com>
2025-05-02 17:36:54 -0300

    Many-to-many Association with JoinTable

--- src/main/java/com/project/spbt/config/TestConfig.java
+++ src/main/java/com/project/spbt/config/TestConfig.java
@@ -49,6 +49,15 @@ public class TestConfig implements CommandLineRunner {
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
         User u1 = new User(null, "Anna Henderson", "annh@gmail.com", "555-778", "hssj2h201ksj");
         User u2 = new User(null, "John Stewart", "js@gmail.com", "555-278", "hj2h201ksj");
 
