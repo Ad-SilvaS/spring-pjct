8448d81729580d57d154343e18144f1531bafaca
Ad-SilvaS <diegosilvaa63@gmail.com>
2025-05-02 01:09:56 -0300

    OrderStatus enum

--- src/main/java/com/project/spbt/config/TestConfig.java
+++ src/main/java/com/project/spbt/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.project.spbt.entities.Order;
 import com.project.spbt.entities.User;
+import com.project.spbt.entities.enums.OrderStatus;
 import com.project.spbt.repositories.OrderRepository;
 import com.project.spbt.repositories.UserRepository;
 
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Anna Henderson", "annh@gmail.com", "555-778", "hssj2h201ksj");
         User u2 = new User(null, "John Stewart", "js@gmail.com", "555-278", "hj2h201ksj");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T09:23:17Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T17:04:42Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T09:23:17Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T17:04:42Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
